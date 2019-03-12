48175a59bb2d42402eb0bed1a30ecdcc8907e4bf
ciszakdamian <ciszakdamian@gmail.com>
2019-03-12 16:31:59 +0100

    add logs and get auto mx

--- imapsync-auto-migrate.sh
+++ imapsync-auto-migrate.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
+#imapsync-auto-migrate.sh
+#Author: Damian Ciszak
+#Contact: ciszakdamian(at)gmail.com
+#LogDir /var/log/imapsync-auto-migrate/
 
+#remove old status file
 rm status.txt
 
+#get value
 host_remote="$1"
-host_lh="$2"
+host_dst="$2"
 random=$(head -200 /dev/urandom | cksum | cut -f1 -d " ")
+mxip=$(dig "$host_remote" MX +short | head -1 | awk '{print $2}' | xargs -i dig +short {})
 
-mkdir -p logi
+#create log
+logdir="/var/log/imapsync-auto-migrate"
+mkdir -p $logdir
+mainlog="$logdir"/main.log
+echo "#DOMAIN: $host_remote MX IP: $mxip DST: $host_dst" >> $mainlog
 
-line=1;
+#use mx as source imap server
+echo -n "Do you want use domain mx record as source IMAP server? y/n: "
+read -r q
+
+if [ "$q" = "y" ]; then
+    host_remote=$mxip
+fi
+
+#line=1;
 while read -r line; do
     mail=$(echo "$line" | awk '{print $1}')
     pass=$(echo "$line" | awk '{print $2}')
     lpass=$(echo "$line" | awk '{print $2}')
 
-    cmd="imapsync --host1 '$host_remote' --host2 '$host_lh' --user1 '$mail' --user2 '$mail' --password1 '$pass' --password2 '$lpass' --ssl1 --logdir logi --logfile '$random'-'$mail'"
-    imapsync="$cmd ; echo \"\$? $mail\" >> status.txt"
+    cmd="imapsync --host1 '$host_remote' --host2 '$host_dst' --user1 '$mail' --user2 '$mail' --password1 '$pass' --password2 '$lpass' --ssl1 --logdir $logdir/logs --logfile '$random'-'$mail'"
+    imapsync="$cmd ; echo \"\$? $mail\" | tee -a $mainlog status.txt > /dev/null"
 
     #set minimum free memory (kB)
     minmem=700000
@@ -24,7 +43,7 @@ while read -r line; do
     memfree=$(grep MemFree /proc/meminfo | awk '{print $2}')
     while [ "$memfree" -le "$minmem"  ] ; do
         memfree=$(grep MemFree /proc/meminfo | awk '{print $2}')
-        echo "memory limit $memfree"
+        echo "work $memfree"
         sleep 1
     done
 
